8eb96ad54d44895af4c86b83359fb0a88adda177
Bruno Farias <brunofarias1@gmail.com>
2018-06-03 19:38:11 -0300

    Created logic for EndDay funcion

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,7 @@
                 </ul>
                 <strong class="navbar-text navbar-right">Funds: {{funds | currency}}</strong>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">End Day</a></li>
+                    <li><a href="#" @click="endDay">End Day</a></li>
                     <li class="dropdown">
                         <a href="#"
                            class="dropdown-toggle"
@@ -32,13 +32,23 @@
 </template>
 
 <script>
+	import {mapActions} from 'vuex'
+
 	export default {
 		name: "Header",
-        computed: {
+		computed: {
 			funds() {
 				return this.$store.getters.funds
-            }
-        }
+			}
+		},
+		methods: {
+			...mapActions([
+				'randomizeStocks'
+			]),
+			endDay() {
+                this.randomizeStocks()
+			}
+		}
 	}
 </script>
 
