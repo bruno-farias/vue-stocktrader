8c9f8e7c3b966f8db3e55ba4ee4b5b065909078c
Bruno Farias <brunofarias1@gmail.com>
2018-06-03 18:39:30 -0300

    Showing current funds on home and header components

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,21 @@
 <template>
-    <h1>The home component</h1>
+    <div>
+        <h1>Trade or View your Portfolio</h1>
+        <h6>You may save and load your data</h6>
+        <h6>Click on 'End Day' to begin a New Day!</h6>
+        <hr>
+        <p>Your funds: {{funds}}</p>
+    </div>
 </template>
 
 <script>
 	export default {
-		name: "Home"
+		name: "Home",
+        computed: {
+			funds() {
+				return this.$store.getters.funds
+            }
+        }
 	}
 </script>
 
