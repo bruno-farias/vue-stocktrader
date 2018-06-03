d5288ccd9599433fafd83b91c0c23aeb38d38913
Bruno Farias <brunofarias1@gmail.com>
2018-06-03 20:19:07 -0300

    Saving data to firebase

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -23,8 +23,8 @@
                            aria-haspopup="true"
                            aria-expanded="false">Save & Load <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="#">Save Data</a></li>
-                            <li><a href="#">Load Data</a></li>
+                            <li><a href="#" @click="saveData">Save Data</a></li>
+                            <li><a href="#" @click="loadData">Load Data</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -54,7 +54,18 @@
 			]),
 			endDay() {
 				this.randomizeStocks()
-			}
+			},
+            saveData() {
+	            const data = {
+		            funds: this.$store.getters.funds,
+		            stocksPortifolio: this.$store.getters.stockPortfolio,
+		            stocks: this.$store.getters.stocks
+	            }
+	            this.$http.put('data.json', data)
+            },
+            loadData() {
+
+            }
 		}
 	}
 </script>
